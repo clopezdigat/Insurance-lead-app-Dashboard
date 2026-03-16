e89d4ee1f73ff34fe92806c32ba243d88f3e1d32
clopezdigat <clopezdigat@gmail.com>
2026-03-16 14:40:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 import pandas as pd
 import gspread
 from datetime import datetime, timedelta
 import pytz
 import plotly.express as px
+
+# Log in 
+hashed_password = stauth.Hasher(['Password']).generate()
+st.write(f"COPY THIS HASH: {hashed_password[0]})
 
 # --- BRANDING & UI CONFIGURATION ---
 st.set_page_config(page_title="Agency Admin", page_icon="📊", layout="wide")
